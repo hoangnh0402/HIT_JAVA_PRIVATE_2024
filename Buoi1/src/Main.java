34f5d1e34497dd4d930ea9f735281bad857c6d9b
hoangnh4204 <hoangnh4204@gmail.com>
2024-10-20 00:34:40 +0700

    update doc buoi2

--- Buoi1/src/Main.java
+++ Buoi1/src/Main.java
@@ -1,29 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
+
+    // Hàm đệ quy tìm ƯCLN (GCD) bằng thuật toán Euclid
+    public static int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        }
+        return gcd(y, x % y);
+    }
+
+    // Hàm đệ quy tìm BCNN (LCM) dựa vào ƯCLN
+    public static int lcm(int x, int y) {
+        return (x * y) / gcd(x, y); //
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a, b, c;
 
-        System.out.print("Nhập độ dài cạnh thứ nhất (a): ");
-        a = scanner.nextInt();
+        // Nhập 3 số nguyên
+        System.out.print("Nhập 3 số tự nhiên a, b, c: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        System.out.print("Nhập độ dài cạnh thứ hai (b): ");
-        b = scanner.nextInt();
+        // Tính ƯCLN của 3 số
+        int gcd_ab = gcd(a, b);
+        int gcd_abc = gcd(gcd_ab, c);
 
-        System.out.print("Nhập độ dài cạnh thứ ba (c): ");
-        c = scanner.nextInt();
+        // Tính BCNN của 3 số
+        int lcm_ab = lcm(a, b);
+        int lcm_abc = lcm(lcm_ab, c);
 
-        if (a > 0 && b > 0 && c > 0) {
-            if (a + b > c && a + c > b && b + c > a) {
-                System.out.println("Ba số " + a + ", " + b + ", " + c + " có thể là độ dài 3 cạnh của một tam giác.");
-            } else {
-                System.out.println("Ba số " + a + ", " + b + ", " + c + " KHÔNG phải là độ dài 3 cạnh của một tam giác.");
-            }
-        } else {
-            System.out.println("Các cạnh của tam giác phải là số dương.");
-        }
+        // In kết quả
+        System.out.println("BCNN của 3 số là: " + lcm_abc);
+        System.out.println("ƯCLN của 3 số là: " + gcd_abc);
 
         scanner.close();
+
+        /*
+        Giả sử chúng ta có các bậc giá như sau:
+
+            0 - 50 kWh: 1.500 VNĐ/kWh
+            51 - 100 kWh: 2.000 VNĐ/kWh
+            101 - 200 kWh: 2.500 VNĐ/kWh
+            201 kWh trở lên: 3.000 VNĐ/kWh
+
+            tính tổng tiền gia đình bạn phải trả khi sử dụng hết 305kWh.
+         */
     }
 }
